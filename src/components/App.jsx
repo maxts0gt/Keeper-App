f9c337ba06e19e59dfbde7efb0bfcff47d5bf262
maxts0gt <bazarsadtsogtjargal@gmail.com>
2021-09-30 17:50:24 +0900

    Google Keeper App

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
+import CreateArea from './CreateArea';
+
+function App() {
+	const [items, setItems] = useState([]);
+
+	function addItem(inputText) {
+		setItems((prevItems) => {
+			return [...prevItems, inputText];
+		});
+	}
+
+	function deleteItem(id) {
+		setItems((prevItems) => {
+			return prevItems.filter((item, index) => {
+				return index !== id;
+			});
+		});
+	}
+
+	return (
+		<div>
+			<Header />
+			<CreateArea onAdd={addItem} />
+			{items.map((noteItem, index) => (
+				<Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteItem} />
+			))}
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
